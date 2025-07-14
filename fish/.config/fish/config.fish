2ee838e5e214b4c1631533038fe7a8e88f8c6be4
Roger <roger23vel@gmail.com>
2025-07-14 14:13:29 -0600

    changes on fish - starship | adding ghostty | add pre commit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,31 +2,41 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Setup initials
+## evals
+eval "$(/opt/homebrew/bin/brew shellenv)"
+## sources
+source ~/.config/local/paths.fish
+
+## sets
+set -g fish_key_bindings fish_vi_key_bindings
+set fish_greeting ""
+
 # A way to reload the shell à la "zsh"
 function fish
     source ~/.config/fish/config.fish
 end
 
-# Disable the fish greeting message
-set fish_greeting ""
-
-# Print a new line after any command
-# source ~/.config/fish/functions/postexec_newline.fish
-
-# Setup brew
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# alias
+# Alias
 alias vi=nvim
+alias d=docker
+alias g=git
+alias lg=lazygit
+alias f=fury
+alias ls='lsd -lah'
+alias dcd='cd ~/dotfiles'
+
+# Abbreviations
+## `ls` → `ls -laG` abbreviation
+abbr -a -g ls ls -laG
+## alias test
+abbr got 'go fmt ./... && goimports -w . && gotestsum --format-hivis --format-hide-empty-pkg'
 
 # Pyenv setup
 # Requires `brew install pyenv`
 if type -q pyenv
     status --is-interactive; and source (pyenv init -|psub)
 end
-
-# `ls` → `ls -laG` abbreviation
-abbr -a -g ls ls -laG
 
 # `ls` → `exa` abbreviation
 # Requires `brew install exa`
@@ -46,4 +56,6 @@ if type -q trash
     abbr --add -g rm trash
 end
 
+# tools
 starship init fish | source
+zoxide init fish | source
