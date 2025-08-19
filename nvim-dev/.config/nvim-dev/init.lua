7d6556a13234571afbca3d96264013608a19d89a
rogervl <roger23vel@gmail.com>
2025-08-18 22:28:12 -0600

    adding treesiter and lsp

--- nvim-dev/.config/nvim-dev/init.lua
+++ nvim-dev/.config/nvim-dev/init.lua
@@ -7,14 +7,5 @@ vim.keymap.set("v", "<space>x",":.lua<CR>")
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 
-
-
-
-
-
-    
-
-
-
-
+-- vim.cmd [[hi @function.builtin.lua guifg=yellow]]
 -- Highlight when yanking text
